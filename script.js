1a1036e45dcbb044ac4f96e516294fe8ee4fb264
Monsejuarez <juarezvilladakatiamonserrath1cpv@cetis50cdmx.com>
2024-02-23 19:00:32 -0600

    se suben los archivos a git

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("hablar").addEventListener("click", () => {
+    decir(document.getElementById("texto").value);
+});
+
+function decir(texto) {
+    const utterance = new SpeechSynthesisUtterance(texto);
+    speechSynthesis.speak(utterance);
+
+    texto.pitch = 2;
+    texto.voives[7];
+    
+}
